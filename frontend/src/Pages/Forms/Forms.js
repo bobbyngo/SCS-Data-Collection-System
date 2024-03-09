8a8ccce5314b932bcdfc5fc4103398a246eaf47f
bobbyngo <ngohuugiabao8980@gmail.com>
2024-03-09 13:12:23 -0500

    example

--- frontend/src/Pages/Forms/Forms.js
+++ frontend/src/Pages/Forms/Forms.js
@@ -1,24 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import './form.css';
-import axios from "axios";
-
-const getForms = () => {
-    axios
-      .get("http://localhost:4000/api/form/all")
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-
-  };
-
+import axios from 'axios';
 
 const Forms = () => {
-    getForms();
-    return ;
-}
+    const [form, setForm] = useState([]);
 
+    const handleDelete = async (id) => {};
+    const getForms = () => {
+        axios
+            .get('http://localhost:4000/api/form/all')
+            .then((res) => {
+                //console.log(res);
+                setForm(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    useEffect(() => {
+        getForms();
+    }, []);
+
+    return (
+        <div className='mt-5'>
+            <table className='table table-striped'>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Form Name</th>
+                        <th>Created At</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {form?.map((item, i) => {
+                        return (
+                            <tr key={item.form_id}>
+                                <td>{item.form_id}</td>
+                                <td>{item.form_name}</td>
+                                <td>{item.createdAt}</td>
+                                <td>
+                                    <i
+                                        className='fa fa-trash-o'
+                                        aria-hidden='true'
+                                        onClick={() => handleDelete(item.id)}
+                                    ></i>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
+};
 
 export default Forms;
